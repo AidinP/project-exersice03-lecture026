bddbd64012ec9d2c733dfa3bc6dd2b9d9502cccb
Aidin Parsiani <aidin.parsiani@gmail.com>
2017-12-05 14:00:43 +0100

    My first commit. I created the test cases for conversor class

---
+++ src/main/java/com/example/maven/project_exersice03_lecture26/ConversorDegreesRadians.java
@@ -0,0 +1,19 @@
+package com.example.maven.project_exersice03_lecture26;
+
+public class ConversorDegreesRadians {
+	//convertDegreesToRadians
+	//convertRadiansToDegrees
+	
+	public double convertDegreesToRadians(double value) {
+		double result = Math.toRadians(value);
+		return result;
+	}
+	
+	public double convertRadiansToDegrees(double value) {
+		double result = Math.toDegrees(value);
+		return result;
+	}
+	
+	
+	
+}
